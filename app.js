98013656cb9dcffde45f63e723d6bdb713bad434
service16 <nwanoziwisdom@gmail.com>
2026-08-19 03:14:46 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getFirestore, collection, addDoc, getDocs, doc, updateDoc, deleteDoc, onSnapshot } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
+import { getFirestore, collection, addDoc, doc, updateDoc, deleteDoc, onSnapshot } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDSzNwHg2LwX1jEMeWwViyOai8yNqu8dpY",
+  authDomain: "udyhotel-1c419.firebaseapp.com",
+  projectId: "udyhotel-1c419",
+  storageBucket: "udyhotel-1c419.firebasestorage.app",
+  messagingSenderId: "714050804978",
+  appId: "1:714050804978:web:a5cb029b2e45069c97c46b",
+  measurementId: "G-FESSMGY7M9"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const db = getFirestore(app);
 
-export { db, collection, addDoc, getDocs, doc, updateDoc, deleteDoc, onSnapshot };
+export { db, collection, addDoc, doc, updateDoc, deleteDoc, onSnapshot };
